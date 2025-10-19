ce470a82f6a4995481b6f50d550c05014f594849
Alex Vera <veraalex97@gmail.com>
2025-10-19 18:15:27 -0400

    Add: template, index.html

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from flask-intro!")
+from flask import Flask, render_template
 
+app = Flask(__name__)
 
-if __name__ == "__main__":
-    main()
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__== "__main__":
+    app.run(debug=True)
